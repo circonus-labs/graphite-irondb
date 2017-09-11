22829b2aa4e28c33128eb7c3a3cd3eb68a6a1878
Riley Berton <riley.berton@circonus.com>
2017-09-11 15:56:54 -0400

    Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='graphite-irondb',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/circonus-labs/graphite-irondb',
     license='BSD',
     author=u'Riley Berton',
