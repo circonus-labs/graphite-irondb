5045eef557464dddad13a70ee0af31d43c7188b5
cody271 <cody271@gmx.com>
2018-04-16 12:08:02 -0700

    setup.py Add build options --pure-python and --with-flatcc

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 # coding: utf-8
+import sys
 from setuptools import setup, find_packages, Extension
 
 FLATCC_PREFIX='/opt/circonus'
-FLATCC_CFLAGS=['-I%s/include'%FLATCC_PREFIX]
-FLATCC_LDFLAGS=['-L%s/lib'%FLATCC_PREFIX,'-Wl,-rpath=%s/lib'%FLATCC_PREFIX,'-lflatccrt']
 
-irondb_flatcc=Extension(
-    'irondb_flatcc',
-    sources=['irondb_flatcc/irondb_flatcc.c'],
-    extra_compile_args=FLATCC_CFLAGS+['-fPIC','-O5','-Wno-strict-prototypes'],
-    extra_link_args=FLATCC_LDFLAGS
-)
+def build_ext(argv, setup_args):
+    pure_python = False
+    with_flatcc = FLATCC_PREFIX    
+    if '--pure-python' in argv:
+        pure_python = True
+        argv.remove('--pure-python')
+    for arg in argv:
+        if arg.startswith('--with-flatcc='):
+            assert pure_python == False, 'Cannot combine options --pure-python and --with-flatcc'
+            with_flatcc = arg.split('=')[1]
+            assert with_flatcc != '', '--with-flatcc option cannot have empty path'
+            argv.remove(arg)
+            break
+    if not pure_python:
+        setup_args['ext_modules']=[Extension(
+            'irondb_flatcc',
+            sources=['irondb_flatcc/irondb_flatcc.c'],
+            extra_compile_args=['-I%s/include'%with_flatcc,'-fPIC','-O5','-Wno-strict-prototypes'],
+            extra_link_args=['-L%s/lib'%with_flatcc,'-Wl,-rpath=%s/lib'%with_flatcc,'-lflatccrt']
+        )]
 
-setup(
+setup_args=dict(
     name='graphite-irondb',
     version='0.0.11',
     url='https://github.com/circonus-labs/graphite-irondb',
@@ -21,8 +34,7 @@ setup(
     author_email='riley.berton@circonus.com',
     description=('A storage backend for graphite-web for using IronDB from Circonus'),
     long_description=open('README.md').read(),
-    py_modules=('irondb',),
-    ext_modules=[irondb_flatcc],
+    py_modules=('irondb','irondb_flatbuf'),
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
@@ -40,3 +52,16 @@ setup(
         'requests',
     )
 )
+
+graphite_irondb_help = '''
+Graphite IRONdb options:
+ --pure-python          Use pure Python code for FlatBuffers
+ --with-flatcc=PREFIX   Path prefix for flatcc library
+'''
+
+if __name__ == '__main__':
+    if '--help' in sys.argv or '-h' in sys.argv:
+        print graphite_irondb_help
+    else:
+        build_ext(sys.argv, setup_args)
+    setup(**setup_args)
