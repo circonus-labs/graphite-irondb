47f6d887088d247f9112d6d19751a7acb1bd7e4a
cody271 <cody271@gmx.com>
2019-01-29 18:03:29 -0800

    Wrap testflatbuffer.py invocations in shell function

--- test/testflatbuffer.sh
+++ test/testflatbuffer.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 TEST=$(dirname $(readlink -f $0))
-PYTHON=/opt/circonus/python27/bin/python
+if [ -z "$PYTHON_BIN" ]
+then
+    PYTHON_BIN="$(command -v python)"
+fi
+testflatbuffer () {
+    $PYTHON_BIN $TEST/testflatbuffer.py "$@"
+}
 
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py create_find_data irondb_find.bin 1024
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py create_get_data irondb_get.bin 1024
+testflatbuffer create_find_data irondb_find.bin 1024
+testflatbuffer create_get_data irondb_get.bin 1024
 
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py read_find_data irondb_find.bin -o > irondb_find_py.json
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py read_find_data irondb_find.bin -c -o > irondb_find_flatcc.json
+testflatbuffer read_find_data irondb_find.bin -o > irondb_find_py.json
+testflatbuffer read_find_data irondb_find.bin -c -o > irondb_find_flatcc.json
 
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py read_get_data irondb_get.bin -o > irondb_get_py.json
-PYTHONPATH=. $PYTHON $TEST/testflatbuffer.py read_get_data irondb_get.bin -c -o > irondb_get_flatcc.json
+testflatbuffer read_get_data irondb_get.bin -o > irondb_get_py.json
+testflatbuffer read_get_data irondb_get.bin -c -o > irondb_get_flatcc.json
 
 diff -qs irondb_find_py.json irondb_find_flatcc.json
 diff -qs irondb_get_py.json irondb_get_flatcc.json
