6ddfe2ab50e8b56379094224ab3dcd0e7e556c08
Brady <brady.woods@circonus.com>
2021-09-29 14:10:25 -0700

    added back missing files

---
+++ docker/python3/src/local_settings.py
@@ -0,0 +1,22 @@
+STORAGE_FINDERS = (
+    'irondb.IRONdbFinder',
+)
+SECRET_KEY = 'circonus'
+TAGDB = 'irondb.IRONdbTagFetcher'
+IRONDB_URLS = (
+    'https://api.circonus.com/irondb/graphite',
+)
+
+# Optional.  You need CIRCONUS_TOKEN if you are using this with the 
+# Circonus Saas or Inside products.  See below.
+# If you are not using Circonus SaaS or Inside you can omit this setting
+CIRCONUS_TOKEN = '@@CIRCONUS_API_KEY@@'
+
+IRONDB_BATCH_SIZE = 250
+IRONDB_USE_DATABASE_ROLLUPS = True
+IRONDB_USE_ACTIVITY_TRACKING = True
+IRONDB_TIMEOUT_MS = 10000
+IRONDB_CONNECTION_TIMEOUT_MS = 3005
+IRONDB_MAX_RETRIES = 2
+IRONDB_QUERY_LOG = False
+IRONDB_URLS_ROTATE = True
