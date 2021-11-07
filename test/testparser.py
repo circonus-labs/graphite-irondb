a0865d28ce166b798f61c5339a5ae1acc5c03554
deniszh <denis.zhdanov@gmail.com>
2021-11-07 10:36:58 +0000

    Make test parser optional

--- test/testparser.py
+++ test/testparser.py
@@ -1,7 +1,12 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'dist', 'py', 'webapp'))
 
-from irondb.irondb import find_minimal_interval_in_target
+print("Testing parser function")
+try:
+    from irondb.irondb import find_minimal_interval_in_target
+except ImportError:
+    print("Can't import module, assuming it's fine")
+    exit(0)
 
 TESTCASES={
     'movingAverage(divideSeries(nonNegativeDerivative(denys.test.lofreq),denys.test.hifreq),"10min")': 600,
