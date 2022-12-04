2dbba5196f42adfc75c7888c1da8cd9b2e1cd1c0
deniszh <denis.zhdanov@gmail.com>
2022-12-04 20:02:08 +0000

    more fixes

--- docker/python2/src/entrypoint.sh
+++ docker/python2/src/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+service nginx start
+source /opt/graphite/bin/activate
 export DJANGO_SETTINGS_MODULE='graphite.settings'
 PYTHONPATH="/opt/graphite/lib/:/opt/graphite/lib/python2.7/site-packages/:/opt/graphite/webapp/" \
     /opt/graphite/bin/gunicorn wsgi \
--- docker/python3/src/entrypoint.sh
+++ docker/python3/src/entrypoint.sh
@@ -1,41 +1,12 @@
 #!/usr/bin/env bash
 
-spinner() {
-    spin='-\|/'
-
-    i=0
-    while kill -0 $1 2>/dev/null
-    do
-        i=$(( (i+1) %4 ))
-        printf "\r working: ${spin:$i:1}"
-        sleep .1
-    done
-    echo ""
-}
-
-echo "Starting nginx" && nginx || exit 1
+service nginx start
 source /opt/graphite/bin/activate
-echo "Cleaning up any pre-existing build artifacts"
-[[ ! -d /graphite-irondb ]] && [[ ! -w /graphite-irondb ]] && exit 1
-cd /graphite-irondb
-[[ -d ./dist ]] && rm -rf ./dist && mkdir -p ./dist/py
-[[ -d ./build ]] && rm -rf ./build
-[[ -f ./graphite_irondb.egg-info ]] && rm -f ./graphite_irondb.egg-info
-echo "Running setup"
-python3 setup.py -q install > /dev/null 2>&1 &
-pid=$!
-spinner $pid
 export DJANGO_SETTINGS_MODULE='graphite.settings'
-cd /graphite-irondb/test
-source testflatbuffer.sh || $(echo >&2 -e "Tests failed!" && exit 1)
-
-
-if [[ $TEST_ONLY = false ]]
-then 
-    PYTHONPATH="/opt/graphite/lib/:/opt/graphite/lib/python3.10/site-packages/:/opt/graphite/webapp/" \
+PYTHONPATH="/opt/graphite/lib/:/opt/graphite/lib/python3.10/site-packages/:/opt/graphite/webapp/" \
     /opt/graphite/bin/gunicorn wsgi \
     --workers=4 \
     --bind=127.0.0.1:8080 \
     --preload \
     --pythonpath=/opt/graphite/webapp/graphite
-fi
+
