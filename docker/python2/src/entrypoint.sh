bea452b6790d95dbe4bdd9d19d196bb394fa2fb5
deniszh <denis.zhdanov@gmail.com>
2022-12-04 20:02:08 +0000

    more fixes
    add path
    
    proper path
    
    last fixes
    
    Sync changes
    
    dos2unix
    
    gunicorn path
    
    gunicorn path

--- docker/python2/src/entrypoint.sh
+++ docker/python2/src/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
+service nginx start
 export DJANGO_SETTINGS_MODULE='graphite.settings'
-PYTHONPATH="/opt/graphite/lib/:/opt/graphite/lib/python2.7/site-packages/:/opt/graphite/webapp/" \
-    /opt/graphite/bin/gunicorn wsgi \
+PYTHONPATH="/opt/graphite/lib/:/usr/local/lib/python2.7/dist-packages/:/usr/local/lib/python2.7/site-packages/:/opt/graphite/webapp/" \
+    /usr/local/bin/gunicorn wsgi \
+	--chdir /opt/graphite/webapp/graphite \
     --workers=4 \
     --bind=127.0.0.1:8080 \
-    --preload \
-    --pythonpath=/opt/graphite/webapp/graphite
-
+    --preload
