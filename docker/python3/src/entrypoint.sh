272ade57cfd3f2510a3680fbbeae2e9e4e470576
deniszh <denis.zhdanov@gmail.com>
2023-03-03 20:12:40 +0000

    Sync changes

--- docker/python3/src/entrypoint.sh
+++ docker/python3/src/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 service nginx start
-source /opt/graphite/bin/activate
 export DJANGO_SETTINGS_MODULE='graphite.settings'
-PYTHONPATH="/opt/graphite/lib/:/opt/graphite/lib/python3.10/site-packages/:/opt/graphite/webapp/" \
+PYTHONPATH="/opt/graphite/lib/:/usr/local/lib/python3.10/dist-packages/:/usr/local/lib/python3.10/site-packages/:/opt/graphite/webapp/" \
     /opt/graphite/bin/gunicorn wsgi \
+    --chdir /opt/graphite/webapp/graphite \
     --workers=4 \
     --bind=127.0.0.1:8080 \
-    --preload \
-    --pythonpath=/opt/graphite/lib/python3.10/site-packages/graphite_web-1.1.10-py3.10.egg/graphite
+    --preload
