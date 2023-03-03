03b11c32f8409bf7a07b6e7fda454a967f616bf4
deniszh <denis.zhdanov@gmail.com>
2023-03-03 20:40:13 +0000

    gunicorn path

--- docker/python3/src/entrypoint.sh
+++ docker/python3/src/entrypoint.sh
@@ -3,7 +3,7 @@
 service nginx start
 export DJANGO_SETTINGS_MODULE='graphite.settings'
 PYTHONPATH="/opt/graphite/lib/:/usr/local/lib/python3.10/dist-packages/:/usr/local/lib/python3.10/site-packages/:/opt/graphite/webapp/" \
-    /opt/graphite/bin/gunicorn wsgi \
+    /usr/local/bin/gunicorn wsgi \
     --chdir /opt/graphite/webapp/graphite \
     --workers=4 \
     --bind=127.0.0.1:8080 \
